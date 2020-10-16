2c54fdc125ad3eab49e560a3d86dd513e6137b7a
unknown <mabopoi@gmail.com>
2020-10-16 03:42:26 -0300

    Game form finished

--- components/NewGameForm.jsx
+++ components/NewGameForm.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import UserContext from '../context/User';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import {
   Stack,
   Button,
@@ -10,17 +13,62 @@ import {
 } from '@chakra-ui/core';
 
 const NewGameForm = () => {
+  const user = useContext(UserContext);
+  const router = useRouter();
+  const [gameData, setGameData] = useState({
+    user: user._id,
+    result: '1-0',
+    userColor: 'White',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const game = await axios.post('api/games', gameData);
+    if (game.status == 201) {
+      router.reload();
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    const newData = {
+      ...gameData,
+      [name]: value,
+    };
+
+    setGameData(newData);
+  };
+
   return (
-    <form>
-      <FormControl>
+    <form onSubmit={handleSubmit}>
+      <FormControl isRequired>
         <FormLabel>Moves</FormLabel>
-        <Input />
+        <Input name='moves' onChange={handleChange} />
         <FormLabel>Opponent</FormLabel>
-        <Input />
+        <Input name='opponent' onChange={handleChange} />
         <FormLabel>Result</FormLabel>
-        <Input />
+        <RadioGroup
+          isInline
+          name='result'
+          onChange={handleChange}
+          defaultValue='1-0'
+        >
+          <Radio variantColor='green' value='1-0'>
+            1-0
+          </Radio>
+          <Radio variantColor='green' value='draw'>
+            Draw
+          </Radio>
+          <Radio variantColor='green' value='0-1'>
+            0-1
+          </Radio>
+        </RadioGroup>
         <FormLabel>Your color</FormLabel>
-        <RadioGroup>
+        <RadioGroup
+          name='userColor'
+          onChange={handleChange}
+          defaultValue='White'
+        >
           <Radio variantColor='green' value='White'>
             White pieces
           </Radio>
@@ -28,11 +76,16 @@ const NewGameForm = () => {
             Black pieces
           </Radio>
         </RadioGroup>
-        <FormLabel>Date</FormLabel>
-        <Input type='date' />
       </FormControl>
+      <FormLabel>Date</FormLabel>
+      <Input type='date' name='date' onChange={handleChange} />
       <Stack justifyContent='right'>
-        <Button>Add</Button>
+        <Button
+          type='submit'
+          isDisabled={gameData.moves && gameData.opponent ? false : true}
+        >
+          Add
+        </Button>
       </Stack>
     </form>
   );
