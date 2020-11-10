0b6336ded56217fcd885ea2fc01725917ac6f98f
unknown <mabopoi@gmail.com>
2020-11-10 01:56:24 -0300

    Dockerizing prod env succesfully

--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@ const config = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   cookieSecret: process.env.COOKIE_SECRET,
+  mongoUrl: process.env.MONGO_URL,
 };
 
 module.exports = { config };
--- middlewares/database.js
+++ middlewares/database.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 import nextConnect from 'next-connect';
+import config from '../config';
 
 const middleware = nextConnect();
 
 function database(req, res, next) {
   const isConnected = mongoose.connection.readyState == 1;
+  const mongoUrl = config.mongoUrl || 'mongodb://localhost:27017/gamesaver';
 
   if (!isConnected) {
-    mongoose.connect('mongodb://localhost:27017/gamesaver', {
+    mongoose.connect(mongoUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
